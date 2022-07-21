32d56c3050a00ecd02e3572d6629204ee2f01a09
Milo Somera <milo.somera@gmail.com>
2022-07-21 12:51:43 +0800

    setup dotenv and package.json type module

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const express = require("express");
+// Required external modules
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+// App variables
+
+if (!process.env.PORT) process.exit(1);
+
+const PORT = process.env.PORT || 8080;
 const app = express();
-const port = 8080;
+
+// App configuration
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Infinite Ecommerce"));
 
-app.listen(port, () => console.log("Infinite Ecommerce"));
+// Server activation
+
+app.listen(PORT, () =>
+  console.log(`Infinite Ecommerce app is listening on port ${PORT}`)
+);
